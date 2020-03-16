783ae385d316c002c3e12d9b5fbf36979a4259db
Routerz007 <35748547+Routerz007@users.noreply.github.com>
2020-03-15 21:44:09 -0400

    Added Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,119 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+    public static final class ArmConstants {
+        public static final int kArmMotorPort = 14;
+        public static final int kArmEncoderStartPosition = 0;
+
+        public static final double kArmUpSpeed = 0.5;
+        public static final double kArmDownSpeed = -0.5;
+        public static final double kArmRestSpeed = 0;
+    }
+
+    public static final class BallMoverConstants {
+        public static final int kMiddleMotorPort = 10;
+        public static final int kFrontMotorPort = 6;
+        public static final int kMiddleWheelMotorPort = 16;
+
+        public static final double kMiddleForwardSpeed = -0.35;
+        public static final double kFrontForwardSpeed = 0.45;
+        public static final double kMiddleWheelForwardSpeed = 0.5;
+
+        public static final double kMiddleReverseSpeed = 0.3;
+        public static final double kFrontReverseSpeed = -0.2;
+
+        public static final double kMiddleRestSpeed = 0;
+        public static final double kFrontRestSpeed = 0;
+        public static final double kMiddleWheelRestSpeed = 0;
+
+        public static final double kMiddleAutoForwardSpeed = -0.3;
+        public static final double kFrontAutoForwardSpeed = 0.2;
+        public static final double kMiddleWheelAutoSpeed = 0.5;        
+    }
+
+    public static final class DriveConstants {
+        public static final int kFrontLeftMotorPort = 1;
+        public static final int kFrontRightMotorPort = 2;
+        public static final int kBackLeftMotorPort = 3;
+        public static final int kBackRightMotorPort = 4;
+    }
+
+    public static final class IntakeConstants {
+        public static final int kIntakeMotorPort = 5;
+        public static final int kBackBeltMotorPort = 9;
+        public static final int kAirCompressorPort = 19;
+
+        public static final int kIntakeSolenoidPort = 19;
+        public static final int kIntakeSolenoidPCM = 1;
+
+        public static final double kIntakeBeltForwardSpeed = 1;
+        public static final double kIntakeBeltReverseSpeed = -1;
+        public static final double kIntakeBeltRestSpeed = 0;
+        
+        public static final double kBackBeltForwardSpeed = 0.65;
+        public static final double kBackBeltReverseSpeed = -0.8;
+        public static final double kBackBeltRestSpeed = 0;
+
+        public static final boolean kIntakeSolenoidDeploy = true;
+        public static final boolean kIntakeSolenoidRetract = false;
+    }
+
+    public static final class LiftConstants {
+        public static final int kLeftMotorPort = 12;
+        public static final int kRightMotorPort = 15;
+
+        public static final int kLeftMotorStartPosition = 0;
+        public static final int kRightMotorStartPosition = 0;
+
+        public static final double kLeftLiftUpSpeed = 0.35;
+        public static final double kRightLiftUpSpeed = 0.4;
+
+        public static final double kLeftLiftDownSpeed = -0.45;
+        public static final double kRightLiftDownSpeed = -0.5;
+
+        public static final double kLeftLiftRestSpeed = 0;
+        public static final double kRightLiftRestSpeed = 0;
+
+        public static final int kLeftLiftEncoderLimit = 160000;
+        public static final int kRightLiftEncoderLimit = 160000;
+    }
+
+    public static final class PivotConstants {
+        public static final int kPivotMotorPort = 11;
+        public static final int kPivotMotorStartPosition = 0;
+
+        public static final int kTopEncoderThreshold = 175;
+        public static final int kBottomEncoderThreshold = 155;
+
+        public static final double kShootForwardSpeed = 0.15;
+
+        public static final double kShootBackwardSpeed = -0.15;
+
+        public static final double kShootRestSpeed = 0;
+    }
+
+    public static final class ShootConstants {
+        public static final int kShootTopMotorPort = 8;
+        public static final int kShootBottomMotorPort = 7;
+        public static final int kShootFlipperPort = 19;
+        public static final int kShootFlipperChannel = 0;
+
+        public static final double kTopShooterForwardSpeed = -1;
+        public static final double kBottomShooterForwardSpeed = 1;
+
+        public static final double kTopShooterRestSpeed = 0;
+        public static final double kBottomShooterRestSpeed = 0; 
+    }
+
+    public static final class WOFConstants {
+        public static final int kSpinnerMotorPort = 13;
+
+        public static final double kSpinnerForwardSpeed = 0.5;
+
+        public static final double kSpinnerBackwardSpeed = -0.5;
+
+        public static final double kSpinnerRestSpeed = 0;
+    }
+
 }
